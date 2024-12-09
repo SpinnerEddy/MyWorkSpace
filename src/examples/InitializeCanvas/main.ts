1597a6422e8b988dbe5753d7014142532185b81f
独楽回しeddy <kiryu1022@gmail.com>
2024-12-10 02:07:24 +0900

    GUIで色を変えられるようにする

--- src/examples/InitializeCanvas/main.ts
+++ src/examples/InitializeCanvas/main.ts
@@ -1,5 +1,6 @@
 import { ColorUtility } from "../../libs/color/ColorUtility";
 import { WebGLUtility } from "../../libs/webgl/WebGLUtility";
+import GUI from "lil-gui";
 
 function main()
 {
@@ -9,8 +10,22 @@ function main()
         return;
     }
 
+    const gui = new GUI();
+
+    const params = {
+        color: '#aa00ff',
+        function() { console.log( 'hi' ) }
+    };
+
+    gui.addColor( params, 'color' ).onChange(
+        function(value : string){
+            gl.clearColor(ColorUtility.hexToColor01(value));
+        }
+    );
+    gui.add(document, 'title');
+
     const gl = new WebGLUtility(canvas);
-    gl.clearColor(ColorUtility.hexToColor01("#f68b1f"));
+    gl.clearColor(ColorUtility.hexToColor01(params.color));
 }
 
 window.onload = main;
