cceff37386692f8986d196564667345cbe26d117
SpinnerEddy <kiryu1022@gmail.com>
2024-12-16 00:49:08 +0900

    行列クラスの基本部分を定義

--- src/libs/math/Matrix.ts
+++ src/libs/math/Matrix.ts
@@ -9,12 +9,38 @@ export class Matrix{
         this.data = new Float32Array(rowNum * colNum).fill(initializeValue);
     }
 
+    static create(sizeNum : number) : Matrix{
+        let matrix = new Matrix(sizeNum, sizeNum);
+        return matrix;
+    }
+
+    static identity(sizeNum : number) : Matrix{
+        let matrix = this.create(sizeNum);
+        for(let i = 0; i < sizeNum; i++){
+            matrix.set(i, i, 1);
+        }
+
+        return matrix;
+    }
+
     get(x : number, y : number) : number{
         return this.data[x * this.colNum + y];
     }
 
     set(x : number, y : number, value : number) : void{
         this.data[x * this.colNum + y] = value;
+    }
+
+    get row() : number{
+        return this.rowNum;
+    }
+
+    get col() : number{
+        return this.colNum;
+    }
+    
+    get isSquareMatrix() : boolean{
+        return this.rowNum == this.colNum;
     }
 
     toArray(): Float32Array{
