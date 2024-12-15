7848d9be431414bf11d32b8f23f90ca7b8293f44
SpinnerEddy <kiryu1022@gmail.com>
2024-12-16 00:37:26 +0900

    行列クラスの基本部分を定義

--- src/libs/math/Matrix.ts
+++ src/libs/math/Matrix.ts
@@ -1,3 +1,23 @@
-class Matrix{
-    
+export class Matrix{
+    private rowNum : number;
+    private colNum : number;
+    private data : Float32Array;
+
+    constructor(rowNum : number, colNum : number, initializeValue: number = 0){
+        this.rowNum = rowNum;
+        this.colNum = colNum;
+        this.data = new Float32Array(rowNum * colNum).fill(initializeValue);
+    }
+
+    get(x : number, y : number) : number{
+        return this.data[x * this.colNum + y];
+    }
+
+    set(x : number, y : number, value : number) : void{
+        this.data[x * this.colNum + y] = value;
+    }
+
+    toArray(): Float32Array{
+        return this.data;
+    }
 }
