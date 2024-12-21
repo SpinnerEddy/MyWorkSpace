3d368f10a2b14a13d6e9e10bc8d41e72fd05c34c
SpinnerEddy <kiryu1022@gmail.com>
2024-12-22 03:09:42 +0900

    行列の乗算の実装とテストを追加

--- src/libs/src/math/MatrixHandler.ts
+++ src/libs/src/math/MatrixHandler.ts
@@ -30,6 +30,52 @@ export class MatrixHandler{
         return result;
     }
 
+    static sub(a : Matrix, b : Matrix) : Matrix{
+        if(!this.checkSizeEqual(a, b)){
+            throw new Error("Not Equal Matrix Dimension. Cannot Calculate!")
+        }
+        
+        const result = new Matrix(a.row, a.col);
+        for(let x = 0; x < a.row; x++){
+            for(let y = 0; y < a.col; y++){
+                result.set(x, y, a.get(x, y) - b.get(x, y));
+            }
+        }
+
+        return result;
+    }
+
+    static multiply(a : Matrix, b : Matrix): Matrix;
+    static multiply(a : Matrix, b : number): Matrix;
+    static multiply(a : Matrix, b : Matrix | number) : Matrix{
+        if(b instanceof Matrix){
+            if(a.col != b.row){
+                throw new Error("Not Equal A Row Number and B Col Number. Cannot Multiply");
+            }
+
+            const result = new Matrix(a.row, b.col);
+            for(let x = 0; x < a.row; x++){
+                for(let y = 0; y < a.col; y++){
+                    let sum = 0;
+                    for(let v = 0; v < a.col; v++){
+                        sum += a.get(x, v) * b.get(v, y);
+                    }
+                    result.set(x, y, sum);
+                }
+            }
+            return result;
+        }
+        else{
+            const result = new Matrix(a.row, a.col);
+            for(let x = 0; x < a.row; x++){
+                for(let y = 0; y < a.col; y++){
+                    result.set(x, y, a.get(x, y) * b);
+                }
+            }
+            return result;
+        }
+    }
+
     static checkSizeEqual(a : Matrix, b : Matrix) : boolean{
         if(a.col != b.col || a.row != b.row){
             console.log(`col : ${a.col},${b.col}`);
--- src/libs/tests/math/MatrixHandlerTest.ts
+++ src/libs/tests/math/MatrixHandlerTest.ts
@@ -19,3 +19,78 @@ test("Matrix Add", () => {
 
     expect(result).toEqual(exceptResult);
 });
+
+test("Matrix Multiply number", () => {
+    let matrixA = MatrixHandler.identity(2);
+
+    let result = MatrixHandler.multiply(matrixA, 5);
+    let exceptResult = new Matrix(2, 2);
+    exceptResult.set(0, 0, 5);
+    exceptResult.set(0, 1, 0);
+    exceptResult.set(1, 0, 0);
+    exceptResult.set(1, 1, 5);
+
+    expect(result).toEqual(exceptResult);
+});
+
+test("Matrix Multiply Matrix", () => {
+    let matrixA = MatrixHandler.identity(2);
+    let matrixB = MatrixHandler.identity(2);
+    matrixA = MatrixHandler.multiply(matrixA, 5);
+    matrixB.set(0, 1, 3);
+    matrixB.set(1, 0, 2);
+    let result = MatrixHandler.multiply(matrixA, matrixB);
+
+    let exceptResult = new Matrix(2, 2);
+    exceptResult.set(0, 0, 5);
+    exceptResult.set(0, 1, 15);
+    exceptResult.set(1, 0, 10);
+    exceptResult.set(1, 1, 5);
+
+    expect(result).toEqual(exceptResult);
+});
+
+test("Matrix Multiply Matrix", () => {
+    let matrixA = MatrixHandler.identity(2);
+    let matrixB = MatrixHandler.identity(2);
+    matrixA = MatrixHandler.multiply(matrixA, 5);
+    matrixB.set(0, 1, 3);
+    matrixB.set(1, 0, 2);
+    let result = MatrixHandler.multiply(matrixA, matrixB);
+
+    let exceptResult = new Matrix(2, 2);
+    exceptResult.set(0, 0, 5);
+    exceptResult.set(0, 1, 15);
+    exceptResult.set(1, 0, 10);
+    exceptResult.set(1, 1, 5);
+
+    expect(result).toEqual(exceptResult);
+});
+
+test("Matrix Multiply Matrix not square", () => {
+    let matrixA = new Matrix(2, 3, 0);
+    let matrixB = new Matrix(3, 2, 0);
+    matrixA.set(0, 0, 1);
+    matrixA.set(0, 1, 2);
+    matrixA.set(0, 2, 3);
+    matrixA.set(1, 0, 1);
+    matrixA.set(1, 1, 2);
+    matrixA.set(1, 2, 3);
+
+    matrixB.set(0, 0, 1);
+    matrixB.set(0, 1, 1);
+    matrixB.set(1, 0, 2);
+    matrixB.set(1, 1, 2);
+    matrixB.set(2, 0, 3);
+    matrixB.set(2, 1, 3);
+
+    let result = MatrixHandler.multiply(matrixA, matrixB);
+
+    let exceptResult = new Matrix(2, 2);
+    exceptResult.set(0, 0, 14);
+    exceptResult.set(0, 1, 14);
+    exceptResult.set(1, 0, 14);
+    exceptResult.set(1, 1, 14);
+
+    expect(result).toEqual(exceptResult);
+});
