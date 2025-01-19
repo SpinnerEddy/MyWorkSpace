1e172e78a49e0c284bb1d99d6ef47627b6be1e1d
SpinnerEddy <kiryu1022@gmail.com>
2025-01-20 00:55:37 +0900

    三角形描画試みまで

--- src/examples/DrawTriangle2D/main.ts
+++ src/examples/DrawTriangle2D/main.ts
@@ -1,5 +1,8 @@
 import { MyColorCode } from "../../libs/src/color/ColorConstants";
 import { ColorUtility } from "../../libs/src/color/ColorUtility";
+import { Matrix } from "../../libs/src/math/matrix/Matrix";
+import { MatrixHandler } from "../../libs/src/math/MatrixHandler";
+import { Vector3 } from "../../libs/src/math/vector/Vector3";
 import { ShaderLoader } from "../../libs/src/webgl/ShaderLoader";
 import { WebGLUtility } from "../../libs/src/webgl/WebGLUtility";
 import GUI from "lil-gui";
@@ -11,6 +14,8 @@ async function main()
         console.error('Not Found Canvas!!');
         return;
     }
+    canvas.width = 500;
+    canvas.height = 500;
 
     const gui = new GUI();
 
@@ -28,11 +33,11 @@ async function main()
 
     const shaderLoader = ShaderLoader.getInstance();
 
+    let vs;
+    let fs;
     try{
-        const vs = await shaderLoader.loadShaderSource('default', 'vert');
-        const fs = await shaderLoader.loadShaderSource('default', 'frag');
-        console.log(vs);
-        console.log(fs);
+        vs = await shaderLoader.loadShaderSource('default', 'vert');
+        fs = await shaderLoader.loadShaderSource('default', 'frag');
     }
     catch(error){
         console.error(error);
@@ -41,7 +46,24 @@ async function main()
     const gl = new WebGLUtility(canvas);
     gl.clearColor(ColorUtility.hexToColor01(params.color));
 
-    
+    const program = gl.createProgram(vs, fs);
+    let vertexPosition = [
+        0.0, 1.0, 0.0,
+        1.0, 0.0, 0.0,
+        -1.0, 0.0, 0.0,
+    ]
+    let vbo = gl.createVbo(new Float32Array(vertexPosition));
+    gl.SetAttributeVboLocation(program, 'aPosition', 3, vbo);
+
+    let mMatrix = MatrixHandler.identity(4);
+    let vMatrix = MatrixHandler.lookAt(new Vector3(0.0, 1.0, 3.0), new Vector3(0.0, 0.0, 0.0), new Vector3(0.0, 1.0, 0.0));
+    let pMatrix = MatrixHandler.perspective(90, canvas.width, canvas.height, 0.1, 100);
+    let mvpMatrix = MatrixHandler.multiply(MatrixHandler.multiply(pMatrix, vMatrix), mMatrix);
+
+    console.log(mvpMatrix);
+    gl.SetUniformMatrix(program, 'mvpMatrix', mvpMatrix);
+    gl.drawArrays(3);
+
 }
 
 main();
