bc2eff77b4f325a9135eeca5d29a6dd5190a1b98
SpinnerEddy <kiryu1022@gmail.com>
2025-01-19 16:40:00 +0900

    三角形描画に向けての準備

---
+++ src/examples/DrawTriangle2D/main.ts
@@ -0,0 +1,31 @@
+import { ColorUtility } from "../../libs/src/color/ColorUtility";
+import { WebGLUtility } from "../../libs/src/webgl/WebGLUtility";
+import GUI from "lil-gui";
+
+function main()
+{
+    const canvas = document.getElementById('webgl-canvas') as HTMLCanvasElement;
+    if(canvas == null){
+        console.error('Not Found Canvas!!');
+        return;
+    }
+
+    const gui = new GUI();
+
+    const params = {
+        color: '#aa00ff',
+        function() { console.log( 'hi' ) }
+    };
+
+    gui.addColor( params, 'color' ).onChange(
+        function(value : string){
+            gl.clearColor(ColorUtility.hexToColor01(value));
+        }
+    );
+    gui.add(document, 'title');
+
+    const gl = new WebGLUtility(canvas);
+    gl.clearColor(ColorUtility.hexToColor01(params.color));
+}
+
+window.onload = main;
