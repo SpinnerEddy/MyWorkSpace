4fb1cba70e902060f06a9800bdb340b37d6ab390
SpinnerEddy <kiryu1022@gmail.com>
2025-02-11 02:35:46 +0900

    新しいベクトルクラスの実装一段落

---
+++ src/libs/src/math/vector_new/VectorConstants.ts
@@ -0,0 +1,8 @@
+import { Vector3 } from "./Vector3";
+
+export const DefaultVectorConstants = 
+{
+    AXIS2DX: new Vector3(1, 0, 0),
+    AXIS2DY: new Vector3(0, 1, 0),
+    AXIS2DZ: new Vector3(0, 0, 1),
+}
