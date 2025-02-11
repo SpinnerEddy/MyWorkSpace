90feebb71003c417f27678d4f90a1b1c7e65575f
SpinnerEddy <kiryu1022@gmail.com>
2025-02-12 01:55:00 +0900

    MatrixUtilityの修正に伴うエラーの解消

--- src/examples/DrawRotateRectangle/main.ts
+++ src/examples/DrawRotateRectangle/main.ts
@@ -1,6 +1,6 @@
 import { MyColorCode } from "../../libs/src/color/ColorConstants";
 import { ColorUtility } from "../../libs/src/color/ColorUtility";
-import { MatrixHandler } from "../../libs/src/math/MatrixUtility";
+import { MatrixUtility } from "../../libs/src/math/MatrixUtility";
 import { Vector3 } from "../../libs/src/math/vector/Vector3";
 import { DefaultVectorConstants } from "../../libs/src/math/vector/VectorConstants";
 import { WebGLUtility } from "../../libs/src/webgl/WebGLUtility";
@@ -60,18 +60,18 @@ function main()
     gl.enableVertexAttribArray(positionLocationIndex);
     gl.vertexAttribPointer(positionLocationIndex, 3, gl.FLOAT, false, 0, 0);
 
-    let modelMatrix = MatrixHandler.identity(4);
-    let viewMatrix = MatrixHandler.lookAt(new Vector3(0.0, 0.0, 3.0), new Vector3(0.0, 0.0, 0.0), new Vector3(0.0, 1.0, 0.0));
-    let projectionMatrix = MatrixHandler.perspective(45, canvas.width, canvas.height, 0.1, 100);
-    let mvpMatrix = MatrixHandler.multiply(MatrixHandler.multiply(projectionMatrix, viewMatrix), modelMatrix);
+    let modelMatrix = MatrixUtility.identity44();
+    let viewMatrix = MatrixUtility.lookAt(new Vector3(0.0, 0.0, 3.0), new Vector3(0.0, 0.0, 0.0), new Vector3(0.0, 1.0, 0.0));
+    let projectionMatrix = MatrixUtility.perspective(45, canvas.width, canvas.height, 0.1, 100);
+    let mvpMatrix = MatrixUtility.multiply(MatrixUtility.multiply(projectionMatrix, viewMatrix), modelMatrix);
 
     const mvpMatrixUniformLocation = gl.getUniformLocation(program, 'mvpMatrix');
     const colorUniformLocation = gl.getUniformLocation(program, 'uColor');
 
     function render(){
         util.clearColor(ColorUtility.hexToColor01(MyColorCode.COLOR_HARUKI));
-        modelMatrix = MatrixHandler.rotate3D(modelMatrix, 0.05, DefaultVectorConstants.AXIS2DZ);
-        mvpMatrix = MatrixHandler.multiply(MatrixHandler.multiply(projectionMatrix, viewMatrix), modelMatrix);
+        modelMatrix = MatrixUtility.rotate3D(modelMatrix, 0.05, DefaultVectorConstants.AXIS2DZ);
+        mvpMatrix = MatrixUtility.multiply(MatrixUtility.multiply(projectionMatrix, viewMatrix), modelMatrix);
 
         gl.uniform3fv(colorUniformLocation, ColorUtility.hexToColor01(MyColorCode.COLOR_SENA).toRGBArray);
         gl.uniformMatrix4fv(mvpMatrixUniformLocation, false, mvpMatrix.toArray());
