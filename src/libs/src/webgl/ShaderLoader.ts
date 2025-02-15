6f3bb25f2bb7ff733e564d14e25cd87686c1e8a2
SpinnerEddy <kiryu1022@gmail.com>
2025-02-16 02:22:51 +0900

    commit

--- src/libs/src/webgl/ShaderLoader.ts
+++ src/libs/src/webgl/ShaderLoader.ts
@@ -1,49 +1,21 @@
 export class ShaderLoader{
-    private static instance: ShaderLoader;
-    private shaderCache: Map<string, string>;
+    private static instance: ShaderLoader | null = null;
+    private static vertexShaderCache: Map<string, string> = new Map();
+    private static fragmentShaderCache: Map<string, string> = new Map();
 
-    private constructor(){
-        this.shaderCache = new Map();
-    }
 
-    static getInstance(): ShaderLoader{
-        if(ShaderLoader.instance == null){
-            ShaderLoader.instance = new ShaderLoader();
+    constructor(){}
+
+    public static getInstance(): ShaderLoader {
+        if(!this.instance){
+            this.instance = new ShaderLoader();
         }
 
-        return ShaderLoader.instance;
+        return this.instance;
     }
 
-    async loadShaderSource(name: string, type: 'vert' | 'frag'): Promise<string>{
-        switch(type){
-            case 'vert':
-                return this.loadVertexShaderSource(name);
-            case 'frag':
-                return this.loadFragmentShaderSource(name);
-            default:
-                throw new Error(`Unknown type shader!! ${name}.${type}`);
-        }
+    public async loadCommonShaders(): Promise<void> {
+
     }
 
-    async loadVertexShaderSource(name: string): Promise<string>{
-        const fileName = `${name}.vert`;
-        if(this.shaderCache.has(fileName)){
-            return this.shaderCache.get(fileName)!;
-        }
-
-        const shaderSource = await import(`./shader/${name}.vert?raw`);
-        this.shaderCache.set(fileName, shaderSource.default);
-        return shaderSource.default;
-    }
-
-    async loadFragmentShaderSource(name: string): Promise<string>{
-        const fileName = `${name}.frag`;
-        if(this.shaderCache.has(fileName)){
-            return this.shaderCache.get(fileName)!;
-        }
-
-        const shaderSource = await import(`./shader/${name}.frag?raw`);
-        this.shaderCache.set(fileName, shaderSource.default);
-        return shaderSource.default;
-    }
 }
